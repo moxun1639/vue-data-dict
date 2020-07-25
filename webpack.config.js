37a765f658c3b12941d77281a6d2a67ec84e0618
moxun1639 <mxnstrive@gmail.com>
2020-07-25 12:03:55 +0800

    fix library name

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'vue-data-dict.js',
-    library: 'MXDICT',
+    library: 'VueDataDict',
     libraryExport: 'default',
     libraryTarget: 'this',
   },
