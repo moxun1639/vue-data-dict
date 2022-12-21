3ec6c873a86591aaa783547ad95a4e5c1c498a33
Moxun1639 <mxnstrive@gmail.com>
2022-12-21 19:10:29 +0800

    add "register"、"wait" methods

--- src/lib/Dict.js
+++ src/lib/Dict.js
@@ -24,28 +24,52 @@ export default class Dict {
       options = { types: options }
     }
     const opts = merge.recursive({}, DEFAULT_DICT_OPTIONS, options)
-    if (opts.types === undefined) {
-      throw new Error('need dict types')
-    }
+
+    this._dictMetas = []
+
     const ps = []
-    this._dictMetas = opts.types.map(t => DictMeta.parse(t))
-    this._dictMetas.forEach(dictMeta => {
-      const type = dictMeta.type
-      Vue.set(this.label, type, {})
-      Vue.set(this.dict, type, [])
-      if (dictMeta.lazy) {
-        return
-      }
-      ps.push(loadDict(this, dictMeta))
+    opts.types.forEach(t => {
+      ps.push(this.register(t))
     })
     return Promise.all(ps)
+  }
+
+  /**
+   * 注册字典
+   * @param {options} type 字典类型
+   */
+  register(options) {
+    const dictMeta = DictMeta.parse(options)
+    this._dictMetas.push(dictMeta)
+    const type = dictMeta.type
+    Vue.set(this.label, type, {})
+    Vue.set(this.dict, type, [])
+    if (dictMeta.lazy) {
+      return
+    }
+    return loadDict(this, dictMeta)
+  }
+
+  wait(type) {
+    const typeDict = this.dict[type]
+    if (!typeDict) {
+      return Promise.reject('no such dict: ' + type)
+    }
+    const loader = typeDict.loader
+    if (loader) {
+      return loader.then(() => {
+        return this.dict[type]
+      })
+    } else {
+      return Promise.resolve(this.dict[type])
+    }
   }
 
   /**
    * 重新加载字典
    * @param {String} type 字典类型
    */
-  reloadDict(type) {
+  reload(type) {
     const dictMeta = this._dictMetas.find(e => e.type === type)
     if (dictMeta === undefined) {
       return Promise.reject(`the dict meta of ${type} was not found`)
@@ -65,21 +89,24 @@ function loadDict(dict, dictMeta) {
   if (!(dictReq instanceof Promise)) {
     dictReq = Promise.resolve(dictReq)
   }
-  return dictReq
-    .then(response => {
-      const type = dictMeta.type
-      let dicts = dictMeta.responseConverter(response, dictMeta)
-      if (!(dicts instanceof Array)) {
-        console.error('the return of responseConverter must be Array.<DictData>')
-        dicts = []
-      } else if (dicts.filter(d => d instanceof DictData).length !== dicts.length) {
-        console.error('the type of elements in dicts must be DictData')
-        dicts = []
-      }
-      dict.dict[type].splice(0, Number.MAX_SAFE_INTEGER, ...dicts)
-      dicts.forEach(d => {
-        Vue.set(dict.label[type], d.value, d.label)
-      })
-      return dicts
+  const loader = dictReq.then(response => {
+    const type = dictMeta.type
+    let dicts = dictMeta.responseConverter(response, dictMeta)
+    if (!(dicts instanceof Array)) {
+      console.error('the return of responseConverter must be Array.<DictData>')
+      dicts = []
+    } else if (dicts.filter(d => d instanceof DictData).length !== dicts.length) {
+      console.error('the type of elements in dicts must be DictData')
+      dicts = []
+    }
+    dict.dict[type].splice(0, Number.MAX_SAFE_INTEGER, ...dicts)
+    dicts.forEach(d => {
+      Vue.set(dict.label[type], d.value, d.label)
     })
+    return dicts
+  })
+  dict.dict[dictMeta.type].loader = loader
+  return loader.finally(function () {
+    delete dict.dict[dictMeta.type].loader
+  })
 }
